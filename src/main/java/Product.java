1a5af030944563ab3e10252be7d93acd4d1a2ffd
asif-kamal <asif.kamal1489@gmail.com>
2026-09-25 14:35:01 -0500

    HAS A relationship with Computer - learning composition

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -18,8 +18,23 @@ class Monitor extends Product {
     }
 
 class MotherBoard extends Product {
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public MotherBoard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        super(model, manufacturer);
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
     public MotherBoard(String model, String manufacturer) {
         super(model, manufacturer);
+    }
+
+    public void loadProgram(String programName) {
+        System.out.println("Loading " + programName + "...");
     }
 }
 
